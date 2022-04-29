ca1b8688e3f22472cd003e88de92500e7acd236a
advait1109 <86603536+advait1109@users.noreply.github.com>
2022-04-29 16:39:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,3 +21,38 @@ function speak(){
     var utterthis=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterthis);
 }
+function identify_emotion(){
+    image=document.getElementById("result_img");
+    classifier.classify(image, gotResult);
+}
+function gotResult(error,result){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById("result_emotion_name").innerHTML=result[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=result[1].label;
+        prediction1=result[0].label;
+        prediction2=result[1].label;
+        speak();
+        if (prediction1=="Happy"){
+            document.getElementById("update_emoji").innerHTML="&#128512;";
+        }
+        if (prediction1=="Angry"){
+            document.getElementById("update_emoji").innerHTML="&#128545;";
+        }
+        if (prediction1=="Sad"){
+            document.getElementById("update_emoji").innerHTML="&#128532;";
+        }
+        if (prediction2=="Happy"){
+            document.getElementById("update_emoji2").innerHTML="&#128512;";
+        }
+        if (prediction2=="Angry"){
+            document.getElementById("update_emoji2").innerHTML="&#128545;";
+        }
+        if (prediction2=="Sad"){
+            document.getElementById("update_emoji2").innerHTML="&#128532;";
+        }
+    }
+}
